de78db1ee45359dc85b289d6326a2f55f0938e48
J. Tanner Porter <jtporter9@gmail.com>
2020-12-03 17:45:03 -0700

    working on mobile menu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,56 +1,76 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo-footer.png'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import logo from '../assets/logo.svg';
+import caret from '../assets/caret-right.svg';
+import bootCountryLogo from '../assets/boot-country-logo.svg';
+
+// import facebook from '../img/social/facebook.svg'
+// import instagram from '../img/social/instagram.svg'
+// import twitter from '../img/social/twitter.svg'
+// import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="My Store"
-            style={{ height: '10em' }}
-          />
+      <footer>
+        <div className="footer-container">
+          <div>
+            <h2 className="white-text">Save 20% off!</h2>
+            <p className="footer-message white-text">Sign up to receive 20% off of your first order as well as deals and happenings from Boot Factory Outlet.</p>
+            <form>
+              <input style={{color: "black"}} type="email" placeholder="Email Address" />
+              <input style={{backgroundImage: "url(" + caret + ")"}} type="submit" value="" />
+            </form>
+          </div>
+          <div>
+            <p className="footer-label white-text">Information</p>
+            <ul>
+              <li>
+                <Link className="white-text" to="/about">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link className="white-text" to="/returns">
+                  Returns
+                </Link>
+              </li>
+              <li>
+                <Link className="white-text" to="/FAQ">
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link className="white-text" to="/help">
+                  Help
+                </Link>
+              </li>
+              <li>
+                <Link className="white-text" to="/stores">
+                  Stores
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-logo-container">
+            <div className="block-logo-container">
+              <div className="split-logos">
+                <Link to="/" title="Logo">
+                  <img className="logo" style={{width: '100px'}} src={logo} alt="JB Dillon" />
+                </Link>
+                <Link to="/" title="Logo">
+                  <img className="logo" src={bootCountryLogo} alt="JB Dillon" />
+                </Link>
+              </div>
+              <p className="sub-text white-text">ⓒ 2020 Boot Factory Outlet. Privacy Policy, Do Not Sell, MAP Policy, Terms.</p>
+            </div>
+          </div>
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter" style={{ paddingBottom: '3em' }}>
-          <div className="container has-background-black has-text-white-ter">
+        {/* <div className="content has-text-centered has-text-white-ter" style={{ paddingBottom: '3em' }}>
+          <div className="container has-text-white-ter">
             <div className="columns">
               <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About Us
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Our Products
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
               </div>
               <div className="column is-4">
                 <section>
@@ -101,7 +121,7 @@ const Footer = class extends React.Component {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </footer>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import logo from '../assets/logo.svg';
+import hamburgerLogo from '../assets/hamburger.svg';
+import searchLogo from '../assets/search.svg';
 import cartIcon from '../assets/cart-icon.svg';
+import caretDown from '../assets/caret-down.svg';
+import upArrowWhite from '../assets/up-arrow-white.svg';
 
 import CartContext from '../context/CartProvider';
 
@@ -35,25 +39,104 @@ const Navbar = class extends React.Component {
   };
 
   render() {
-
     return (
       <header>
         <div className="navbar-menu" role="navigation" aria-label="main-navigation"> 
-          <div className="one-quarter logo-container">
-            <Link to="/" title="Logo">
-              <img className="logo" src={logo} alt="JB Dillon" />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}>
-              <span />
-              <span />
-              <span />
+          <div className="logo-container">
+            <div className="mobile-menu">
+              <img src={hamburgerLogo} alt="Mobile Menu" 
+                className={`pointer ${this.state.navBarActiveClass}`}
+                data-target="navMenu"
+                onClick={() => this.toggleHamburger()} />
+              <div className={`opqaque-background ${this.state.navBarActiveClass}`} onClick={() => this.toggleHamburger()}></div>
+              <div className={`mobile-menu-drawer ${this.state.navBarActiveClass}`}>
+                <div className="mobile-search-container">
+                  <input className="mobile-search" placeholder="Search" />
+                </div>
+                <div className="mobile-menu-dropdown-container">
+                  <span>Mens <img src={caretDown} /></span>
+                  <ul>
+                    <li></li>
+                  </ul>
+                </div>
+                <div className="mobile-menu-dropdown-container">
+                  <span>Womens <img src={caretDown} /></span>
+                  <ul>
+                    <li></li>
+                  </ul>
+                </div>
+                <div className="mobile-menu-dropdown-container">
+                  <span>Kids <img src={caretDown} /></span>
+                  <ul>
+                    <li></li>
+                  </ul>
+                </div>
+                <div className="mobile-menu-dropdown-container">
+                  <span>Accessories <img src={caretDown} /></span>
+                  <ul>
+                    <li></li>
+                  </ul>
+                </div>
+                <div className="mobile-menu-links">
+                  <Link to="/womens">
+                    Stores
+                  </Link>
+                </div>
+                <div className="mobile-menu-links">
+                  <Link to="/womens">
+                    About
+                  </Link>
+                </div>
+                <div className="mobile-menu-links">
+                  <Link to="/womens">
+                    Help
+                  </Link>
+                </div>
+                <div className="mobile-menu-links">
+                  <Link to="/womens">
+                    Account
+                  </Link>
+                </div>
+                <div className="mobile-menu-checkout-container">
+                  <CartContext.Consumer>
+                    {value => {
+                    return (
+                          <Link className="navbar-item menu-item-bigcommerce-cart cart-icon-container" to="/cart">
+                            <button>
+                              <img src={upArrowWhite} />
+                              <span>View Cart</span> 
+                              <span>( {value.state.cart.numberItems} )</span>
+                            </button>
+                          </Link>
+                          );
+                        }}
+                  </CartContext.Consumer>
+                </div>
+              </div>
+            </div>
+            <div>
+              <Link to="/" title="Logo">
+                <img className="logo" src={logo} alt="JB Dillon" />
+              </Link>
+            </div>
+            <div className="mobile-cart">
+              <img src={searchLogo} alt="Search" 
+                className={`search-icon pointer ${this.state.navBarActiveClass}`}
+                data-target=""
+                onClick={() => this.toggleHamburger()} />
+              <CartContext.Consumer>
+                {value => {
+                return (
+                      <Link className="navbar-item menu-item-bigcommerce-cart cart-icon-container" to="/cart">
+                        <img src={cartIcon} alt="Cart" />
+                        <span className="bigcommerce-cart__item-count full">{value.state.cart.numberItems}</span>
+                      </Link>
+                      );
+                    }}
+              </CartContext.Consumer>
             </div>
           </div>
-          <nav className="one-half">
+          <nav className="desktop-links">
             <div className="navbar-menu has-text-centered">
               <Link className="menu-link" to="/mens">
                 Mens
@@ -84,112 +167,33 @@ const Navbar = class extends React.Component {
               </Link> */}
             </div>
           </nav>
-          <div className="one-quarter">
-            <div className="navbar-end">
-              <Link className="menu-link" to="/search">
-                Search
-              </Link>
-              <Link className="menu-link" to="/help">
-                About
-              </Link>
-              <Link className="menu-link" to="/login">
-                Login/Register
-              </Link>
-              <CartContext.Consumer>
-                {value => {
-                return (
-                      <Link className="navbar-item menu-item-bigcommerce-cart cart-icon-container" to="/cart">
-                        <img src={cartIcon} alt="Cart" />
-                        {value &&
-                          value.state.cart &&
-                          value.state.cart.numberItems > 0 && (
-                            <span className="bigcommerce-cart__item-count full">{value.state.cart.numberItems}</span>
-                          )}
-                      </Link>
-                      );
-                    }}
-              </CartContext.Consumer>
-            </div>
+          <div className="navbar-right">
+            <Link className="menu-link" to="/search">
+              Search
+            </Link>
+            <Link className="menu-link" to="/help">
+              About
+            </Link>
+            <Link className="menu-link" to="/login">
+              Login/Register
+            </Link>
+            <CartContext.Consumer>
+              {value => {
+              return (
+                    <Link className="navbar-item menu-item-bigcommerce-cart cart-icon-container" to="/cart">
+                      <img src={cartIcon} alt="Cart" />
+                      {value &&
+                        value.state.cart &&
+                        value.state.cart.numberItems > 0 && (
+                          <span className="bigcommerce-cart__item-count full">{value.state.cart.numberItems}</span>
+                        )}
+                    </Link>
+                    );
+                  }}
+            </CartContext.Consumer>
           </div>
         </div>
       </header>
-      // <header>
-      //   <nav
-      //     role="navigation"
-      //     aria-label="main-navigation">
-      //     <div className="container navbar-menu">
-      //       <div className="navbar-brand">
-      //         <Link to="/" title="Logo">
-      //           <img src={logo} alt="JB Dillon" />
-      //         </Link>
-      //         {/* Hamburger menu */}
-      //         <div
-      //           className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-      //           data-target="navMenu"
-      //           onClick={() => this.toggleHamburger()}>
-      //           <span />
-      //           <span />
-      //           <span />
-      //         </div>
-      //       </div>
-      //       <div
-      //         id="navMenu"
-      //         className={`navbar-menu ${this.state.navBarActiveClass}`}>
-      //         <div className="navbar-menu has-text-centered">
-      //         <Link to="/mens">
-      //             Mens
-      //           </Link>
-      //           <Link to="/womens">
-      //             Womens
-      //           </Link>
-      //           <Link to="/accessories">
-      //             Accessories
-      //           </Link>
-      //           <Link to="/stores">
-      //             Stores
-      //           </Link>
-      //           {/* <Link to="/about">
-      //             About
-      //           </Link> */}
-      //           {/* <Link to="/products">
-      //             Products
-      //           </Link>
-      //           <Link to="/blog">
-      //             Blog
-      //           </Link>
-      //           <Link to="/contact">
-      //             Contact
-      //           </Link> */}
-      //         </div>
-      //         <div className="navbar-end has-text-centered">
-      //           <Link to="/search">
-      //             Search
-      //           </Link>
-      //           <Link to="/help">
-      //             About
-      //           </Link>
-      //           <Link to="/login">
-      //             Login/Register
-      //           </Link>
-      //           <CartContext.Consumer>
-      //             {value => {
-      //               return (
-      //                 <Link className="navbar-item menu-item-bigcommerce-cart cart-icon-container" to="/cart">
-      //                   <img src={cartIcon} alt="Cart" />
-      //                   {value &&
-      //                     value.state.cart &&
-      //                     value.state.cart.numberItems > 0 && (
-      //                       <span className="bigcommerce-cart__item-count full">{value.state.cart.numberItems}</span>
-      //                     )}
-      //                 </Link>
-      //               );
-      //             }}
-      //           </CartContext.Consumer>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </nav>
-      // </header>
     );
   }
 };
