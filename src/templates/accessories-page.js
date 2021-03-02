ba5fa6ece617822182f7bde8de7d6652d11c96c8
JT Porter <jtporter@JTs-MacBook-Pro.local>
2021-03-02 14:55:28 -0700

    trying to get publsih

--- src/templates/accessories-page.js
+++ src/templates/accessories-page.js
@@ -9,7 +9,8 @@ export const AccessoriesPageTemplate = ({
   title,
   heading,
   description,
-  products
+  products,
+  brands
 }) => (
   <ProductPageContainer 
     image={image}
@@ -17,6 +18,7 @@ export const AccessoriesPageTemplate = ({
     heading={heading}
     description={description}
     products={products}
+    brands={brands}
   />
 );
 
@@ -40,6 +42,7 @@ const AccessoriesPage = ({ data }) => {
   }
 
   const products = data.allBigCommerceProducts.nodes;
+  const brands = data.allBigCommerceBrands.edges;
 
   return (
     <Layout>
@@ -49,6 +52,7 @@ const AccessoriesPage = ({ data }) => {
         heading={frontmatter.heading}
         description={frontmatter.description}
         products={products}
+        brands={brands}
       />
     </Layout>
   );
