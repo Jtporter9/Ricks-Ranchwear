fe85e45bb5b8356bacadfd44c4dae96146742a52
JT Porter <jtporter@JTs-MacBook-Pro.local>
2021-03-07 23:49:24 -0700

    all filter functinality finally working, a lot of design tweaks to come

--- src/components/productPageContainer/productPageContainer.js
+++ src/components/productPageContainer/productPageContainer.js
@@ -11,7 +11,7 @@ import filterIcon from '../../assets/filter.svg';
 import infoIcon from '../../assets/info-icon.svg';
 import groupedBoots from '../../assets/grouped-boots.svg';
 import CloseIcon from '../../assets/close-icon.svg';
-
+import { number } from 'prop-types';
 
 export default function ProductPageContainer({
     image,
@@ -72,20 +72,21 @@ export default function ProductPageContainer({
     // STATES
     const [filterDrawerOpen, setFilterDrawerOpen] = useState(false);
     const [filterDrawerActiveClass, setFilterDrawerActiveClass] = useState('');
-    const [filter, setFilter] = useState(optionsList[0]);
+    const [filter, setFilter] = useState(null);
     const [brandsFilter, setBrandsFilter] = useState([]);
-    const [colorFilter, setColorFilter] = useState(null);
-    const [widthFilter, setWidthFilter] = useState(null);
-    const [sizeFilter, setSizeFilter] = useState(null);
+    const [colorFilter, setColorFilter] = useState([]);
+    const [widthFilter, setWidthFilter] = useState([]);
+    const [sizeFilter, setSizeFilter] = useState([]);
 
-    const [materialFilter, setMaterialFilter] = useState(null);
-    const [toeStyleFilter, setToeStyleFilter] = useState(null);
-    const [styleNumberFilter, setStyleNumberFilter] = useState(null);
+    const [materialFilter, setMaterialFilter] = useState([]);
+    const [toeStyleFilter, setToeStyleFilter] = useState([]);
+    const [styleNumberFilter, setStyleNumberFilter] = useState([]);
 
     const [isSticky, setSticky] = useState(false);
     const [activeInfoModal, setActiveInfoModal] = useState(false);
     const [isMobileView, setIsMobileView] = useState(false);
-    const [filteredProducts, setFilteredProducts] = useState(products);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [numberOfFilters, setNumberOfFilters] = useState(null);
 
     function toggleFilterDrawer() {
         setFilterDrawerOpen(!filterDrawerOpen);
@@ -93,11 +94,6 @@ export default function ProductPageContainer({
             ? setFilterDrawerActiveClass('filter-drawer-open')
             : setFilterDrawerActiveClass('')
     }
-
-    // FILTER PRODUCTS
-    filter === optionsList[0] && filteredProducts.sort((a, b) => (a.bigcommerce_id > b.bigcommerce_id) ? 1 : -1)
-    filter === optionsList[1] && filteredProducts.sort((a, b) => (a.price > b.price) ? 1 : -1)
-    filter === optionsList[2] && filteredProducts.sort((a, b) => (a.price < b.price) ? 1 : -1)
 
     const ref = useRef(null);
     const handleScroll = () => {
@@ -107,36 +103,55 @@ export default function ProductPageContainer({
     };
 
     function getProductsFilteredByBrand(brands) {
-        brands.length > 1 ? 
-        brands.map(brand => setFilteredProducts([...filteredProducts, ...products.filter(product => product.brand.name === brand)])) : 
-        brands.map(brand => setFilteredProducts(products.filter(product => product.brand.name === brand)));
-        brands.length === 0 && setFilteredProducts(products);
+        brands.length > 1 ? brands.map(brand => setFilteredProducts([...new Set(findDuplicates([...filteredProducts, ...products.filter(product => product.brand.name === brand)]))])) : 
+        brands.map(brand => setFilteredProducts(([...new Set(findDuplicates(products.filter(product => product.brand.name === brand)))])));
+        console.log(brands.length)
+        // brands.length === 0 && setFilteredProducts(products);
     }
 
-    function getProductsFilteredByColor(colors) {
-        colors.length > 1 ? 
-        colors.map(brand => setFilteredProducts([...filteredProducts, ...products.filter(product => product.brand.name === brand)])) : 
-        colors.map(brand => setFilteredProducts(products.filter(product => product.brand.name === brand)));
-        colors.length === 0 && setFilteredProducts(products);
+    function getProductsFilteredByVariant(arr) {
+        arr.length > 1 ?
+            arr.map(item => setFilteredProducts([...new Set(findDuplicates([...filteredProducts, ...products.filter(product => product.variantsList.indexOf(item) === -1 ? false : true)]))])) :
+            arr.map(item => setFilteredProducts([...new Set(findDuplicates(products.filter(product => product.variantsList.indexOf(item) === -1 ? false : true)))]));
+        // arr.length === 0 && setFilteredProducts(products);
     }
 
     useEffect(() => {
+        setNumberOfFilters(
+            brandsFilter.length +
+            colorFilter.length +
+            sizeFilter.length +
+            widthFilter.length +
+            materialFilter.length +
+            toeStyleFilter.length +
+            styleNumberFilter.length
+        );
+        console.log(numberOfFilters);
+        filteredProducts.length === 0 && setFilteredProducts(products);
+        numberOfFilters === 0 && setFilteredProducts(products);
+        (!filter && filteredProducts.length !== 0) && setFilter(optionsList[0]);
+
         window.addEventListener('load', setIsMobileView(window.matchMedia('(max-width: 1000px)').matches));
         window.addEventListener('resize', setIsMobileView(window.matchMedia('(max-width: 1000px)').matches));
         window.addEventListener('scroll', handleScroll);
 
-        brandsFilter && getProductsFilteredByBrand(brandsFilter)
-        colorFilter && setFilteredProducts(products.filter(product => product.variantsList.indexOf(colorFilter) === -1 ? false : true))
-        sizeFilter && setFilteredProducts(products.filter(product => product.variantsList.indexOf(sizeFilter) === -1 ? false : true))
-        widthFilter && setFilteredProducts(products.filter(product => product.variantsList.indexOf(widthFilter) === -1 ? false : true))
-        materialFilter && setFilteredProducts(products.filter(product => product.variantsList.indexOf(materialFilter) === -1 ? false : true))
-        toeStyleFilter && setFilteredProducts(products.filter(product => product.variantsList.indexOf(toeStyleFilter) === -1 ? false : true))
-        styleNumberFilter && setFilteredProducts(products.filter(product => product.variantsList.indexOf(styleNumberFilter) === -1 ? false : true))
+        // FILTER PRODUCTS
+        (filter === optionsList[0] && filteredProducts) && filteredProducts.sort((a, b) => (a.bigcommerce_id > b.bigcommerce_id) ? 1 : -1);
+        (filter === optionsList[1] && filteredProducts) && filteredProducts.sort((a, b) => (a.price > b.price) ? 1 : -1);
+        (filter === optionsList[2] && filteredProducts) && filteredProducts.sort((a, b) => (a.price < b.price) ? 1 : -1);
+
+        brandsFilter.length > 0 && getProductsFilteredByBrand(brandsFilter);
+        colorFilter.length > 0 && getProductsFilteredByVariant(colorFilter);
+        sizeFilter.length > 0 && getProductsFilteredByVariant(sizeFilter);
+        widthFilter.length > 0 && getProductsFilteredByVariant(widthFilter);
+        materialFilter.length > 0 && getProductsFilteredByVariant(materialFilter);
+        toeStyleFilter.length > 0 && getProductsFilteredByVariant(toeStyleFilter);
+        styleNumberFilter.length > 0 && getProductsFilteredByVariant(styleNumberFilter);
 
         return () => {
             window.removeEventListener('scroll', () => handleScroll);
         };
-    }, [brandsFilter, colorFilter, sizeFilter, widthFilter, materialFilter, toeStyleFilter, styleNumberFilter]);
+    }, [numberOfFilters, filter, brandsFilter, colorFilter, sizeFilter, widthFilter, materialFilter, toeStyleFilter, styleNumberFilter]);
 
     return (
         <div className="product-page">
@@ -166,7 +181,7 @@ export default function ProductPageContainer({
                             </div>
                         </div>
                         <div className="products-header-split">
-                            <span style={{ display: 'none' }} className="numbered-products-results">{filteredProducts.length} Results</span>
+                            <span style={{ display: 'none' }} className="numbered-products-results">{filteredProducts ? filteredProducts.length : 0} Results</span>
                         </div>
                         <div className="products-header-split">
                             <Dropdown
@@ -192,46 +207,46 @@ export default function ProductPageContainer({
                                     </div>
                                     {brandsFilter && brandsFilter.map((brand, i) => (
                                         <div key={i} className="filter-swatch">
-                                            <img src={CloseIcon} alt="remove" onClick={() => { setBrandsFilter(brandsFilter.filter(a => a !== brand)); getProductsFilteredByBrand(brandsFilter.filter(a => a !== brand)); }} />
+                                            <img src={CloseIcon} alt="remove" onClick={() => setBrandsFilter(brandsFilter.filter(a => a !== brand))} />
                                             <span>{brand}</span>
                                         </div>
                                     ))}
-                                    {colorFilter && (
-                                        <div className="filter-swatch">
-                                            <img src={CloseIcon} alt="remove" onClick={() => { setColorFilter(null); setFilteredProducts(products); }} />
-                                            <span>{colorFilter}</span>
+                                    {colorFilter && colorFilter.map((color, i) => (
+                                        <div key={i} className="filter-swatch">
+                                            <img src={CloseIcon} alt="remove" onClick={() => { setColorFilter(colorFilter.filter(a => a !== color)); getProductsFilteredByVariant(colorFilter.filter(a => a !== color)); }} />
+                                            <span>{color}</span>
                                         </div>
-                                    )}
-                                    {widthFilter && (
-                                        <div className="filter-swatch">
-                                            <img src={CloseIcon} alt="remove" onClick={() => { setWidthFilter(null); setFilteredProducts(products); }} />
-                                            <span>{widthFilter}</span>
+                                    ))}
+                                    {widthFilter && widthFilter.map((width, i) => (
+                                        <div key={i} className="filter-swatch">
+                                            <img src={CloseIcon} alt="remove" onClick={() => { setWidthFilter(widthFilter.filter(a => a !== width)); getProductsFilteredByVariant(widthFilter.filter(a => a !== width)); }} />
+                                            <span>{width}</span>
                                         </div>
-                                    )}
-                                    {sizeFilter && (
-                                        <div className="filter-swatch">
-                                            <img src={CloseIcon} alt="remove" onClick={() => { setSizeFilter(null); setFilteredProducts(products); }} />
-                                            <span>{sizeFilter}</span>
+                                    ))}
+                                    {sizeFilter && sizeFilter.map((size, i) => (
+                                        <div key={i} className="filter-swatch">
+                                            <img src={CloseIcon} alt="remove" onClick={() => { setSizeFilter(sizeFilter.filter(a => a !== size)); getProductsFilteredByVariant(sizeFilter.filter(a => a !== size)); }} />
+                                            <span>{size}</span>
                                         </div>
-                                    )}
-                                    {materialFilter && (
-                                        <div className="filter-swatch">
-                                            <img src={CloseIcon} alt="remove" onClick={() => { setMaterialFilter(null); setFilteredProducts(products); }} />
-                                            <span>{materialFilter}</span>
+                                    ))}
+                                    {materialFilter && materialFilter.map((material, i) => (
+                                        <div key={i} className="filter-swatch">
+                                            <img src={CloseIcon} alt="remove" onClick={() => { setMaterialFilter(materialFilter.filter(a => a !== material)); getProductsFilteredByVariant(materialFilter.filter(a => a !== material)); }} />
+                                            <span>{material}</span>
                                         </div>
-                                    )}
-                                    {toeStyleFilter && (
-                                        <div className="filter-swatch">
-                                            <img src={CloseIcon} alt="remove" onClick={() => { setToeStyleFilter(null); setFilteredProducts(products); }} />
-                                            <span>{toeStyleFilter}</span>
+                                    ))}
+                                    {toeStyleFilter && toeStyleFilter.map((toeStyle, i) => (
+                                        <div key={i} className="filter-swatch">
+                                            <img src={CloseIcon} alt="remove" onClick={() => { setToeStyleFilter(toeStyleFilter.filter(a => a !== toeStyle)); getProductsFilteredByVariant(toeStyleFilter.filter(a => a !== toeStyle)); }} />
+                                            <span>{toeStyle}</span>
                                         </div>
-                                    )}
-                                    {styleNumberFilter && (
-                                        <div className="filter-swatch">
-                                            <img src={CloseIcon} alt="remove" onClick={() => { setStyleNumberFilter(null); setFilteredProducts(products); }} />
+                                    ))}
+                                    {styleNumberFilter && styleNumberFilter.map((style, i) => (
+                                        <div key={i} className="filter-swatch">
+                                            <img src={CloseIcon} alt="remove" onClick={() => { setStyleNumberFilter(styleNumberFilter.filter(a => a !== style)); getProductsFilteredByVariant(styleNumberFilter.filter(a => a !== style)); }} />
                                             <span>{styleNumberFilter}</span>
                                         </div>
-                                    )}
+                                    ))}
                                 </div>
                                 <Dropdown
                                     dropDownClasses={{ head: 'products-side-filter-head', optionContainer: 'side-filter-dropdown-container' }}
@@ -244,42 +259,42 @@ export default function ProductPageContainer({
                                     dropDownClasses={{ head: 'products-side-filter-head', optionContainer: 'side-filter-dropdown-container' }}
                                     placeholder="Material"
                                     value="Material"
-                                    onChange={v => setMaterialFilter(v)}
+                                    onChange={v => setMaterialFilter([...materialFilter, v])}
                                     options={materialOptions}
                                 />
                                 <Dropdown
                                     dropDownClasses={{ head: 'products-side-filter-head', optionContainer: 'side-filter-dropdown-container' }}
                                     placeholder="Color"
                                     value="Color"
-                                    onChange={v => setColorFilter(v)}
+                                    onChange={v => setColorFilter([...colorFilter, v])}
                                     options={colorOptions}
                                 />
                                 <Dropdown
                                     dropDownClasses={{ head: 'products-side-filter-head', optionContainer: 'side-filter-dropdown-container' }}
                                     placeholder="Width"
                                     value="Width"
-                                    onChange={v => setWidthFilter(v)}
+                                    onChange={v => setWidthFilter([...widthFilter, v])}
                                     options={widthOptions}
                                 />
                                 <Dropdown
                                     dropDownClasses={{ head: 'products-side-filter-head', optionContainer: 'side-filter-dropdown-container' }}
                                     placeholder="Size"
                                     value="Size"
-                                    onChange={v => setSizeFilter(v)}
+                                    onChange={v => setSizeFilter([...sizeFilter, v])}
                                     options={sizeOptions}
                                 />
                                 <Dropdown
                                     dropDownClasses={{ head: 'products-side-filter-head', optionContainer: 'side-filter-dropdown-container' }}
                                     placeholder="Toe Shape"
                                     value="Toe Shape"
-                                    onChange={v => setToeStyleFilter(v)}
+                                    onChange={v => setToeStyleFilter([...toeStyleFilter, v])}
                                     options={toeStyleOptions}
                                 />
                                 <Dropdown
                                     dropDownClasses={{ head: 'products-side-filter-head', optionContainer: 'side-filter-dropdown-container' }}
                                     placeholder="Style"
                                     value="Style"
-                                    onChange={v => setStyleNumberFilter(v)}
+                                    onChange={v => setStyleNumberFilter([...styleNumberFilter, v])}
                                     options={styleNumberOptions}
                                 />
 
@@ -287,7 +302,7 @@ export default function ProductPageContainer({
                             </div>
                         </div>
                         <div className={`bc-product-grid bc-product-grid--archive ${isMobileView ? 'bc-product-grid--4col' : 'bc-product-grid--3col'}`}>
-                            {filteredProducts.map(product => (
+                            {filteredProducts && filteredProducts.map(product => (
                                 <ProductCard key={product.id} product={product} />
                             ))}
                         </div>
