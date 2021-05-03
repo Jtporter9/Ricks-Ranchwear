8ef9057c510a474dc55ec38e5f9627eda6b2e602
dakotaRuller <dakotajru@gmail.com>
2021-05-02 22:23:55 -0600

    Adding default shared page

---
+++ src/templates/shared-content.js
@@ -0,0 +1,12 @@
+//Node Modules
+import React from "react";
+import * as router from "@reach/router";
+
+const SharedContent = () => (
+  <>
+    {console.log(router)}
+    <p>Shared Content</p>
+  </>
+);
+
+export default SharedContent;
