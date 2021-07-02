2185c14a848c563ccf39bc57ebf34b3963ed3870
JT Porter <jtporter@JTs-MacBook-Pro.local>
2021-07-01 18:00:02 -0600

    password changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -75,7 +75,7 @@ const TemplateWrapper = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Helmet>
       <Notify />
-      {password !== 'jbdillon' && (
+      {password !== 'twofreeboots21' && (
         <div className="site-protection-container">
           <img src={logo} alt="Boot Factory" />
           <h2>Enter password to access site:</h2>
