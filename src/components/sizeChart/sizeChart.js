0adfb6354282e86471c9030b154fb3a5508a4304
dakotaRuller <dakotajru@gmail.com>
2021-07-02 12:52:02 -0600

    Adding kids size chart and switch functionality

--- src/components/sizeChart/sizeChart.js
+++ src/components/sizeChart/sizeChart.js
@@ -1,11 +1,35 @@
-import React from 'react'
+
+import React, {useState} from 'react'
 
 //ASSESTS
 import CloseIconBlack from '../../assets/close-icon-black.svg';
 
 export default function SizeChart(props) {
-    const { activeSizeChart, setActiveSizeChart } = props
-    const inches = [
+    const { activeSizeChart, setActiveSizeChart, defaultToAdult = true } = props;
+
+    const [adultChart, setAdultChart] = useState(defaultToAdult ? true : false);
+
+    const kidSizes = [
+      '3',
+      '3',
+      '4',
+      '4',
+      '4',
+      '5',
+      '5',
+      '5',
+      '6',
+      '6',
+      '6',
+      '7',
+      '7',
+      '7',
+      '8',
+      '8',
+      '8',
+    ];
+
+    const adultSizes = [
         '9',
         '9',
         '9',
@@ -23,7 +47,10 @@ export default function SizeChart(props) {
         '11',
         '12',
         '12'
-    ]
+    ];
+
+    const inches = adultChart ? adultSizes : kidSizes;
+
     const inchesFraction = [
         '1/4',
         '1/2 ',
@@ -125,9 +152,20 @@ export default function SizeChart(props) {
             { activeSizeChart && (
                 <div className="size-chart-modal-opaque-background">
                     <div className="size-chart-modal">
+                        <span className="margin-block"/>
                         <div className="size-chart-head">
                             <h3>Size Chart</h3>
                             <img src={CloseIconBlack} alt="close icon" onClick={() => setActiveSizeChart(false)} />
+                        </div>
+                        <div className="chart-switcher">
+                            <p
+                              className={adultChart ? "active" : ""}
+                              onClick={() => setAdultChart(true)}
+                              tabIndex="0">Adult</p>
+                            <p
+                              className={adultChart ? "" : "active"}
+                              onClick={() => setAdultChart(false)}
+                              tabIndex="0">Kids</p>
                         </div>
                         <table>
                             <thead>
@@ -143,8 +181,8 @@ export default function SizeChart(props) {
                                 {inches.map((value, i) => (
                                     <tr>
                                         {i % 2 === 0 ?
-                                            <td>{value}<sup>{inchesFraction[i]}</sup></td> :
-                                            <td className="even-dark-background">{value}<sup>{inchesFraction[i]}</sup></td>
+                                            <td className="inches-display">{value}<sup>{inchesFraction[i]}</sup></td> :
+                                            <td className="even-dark-background ">{value}<sup>{inchesFraction[i]}</sup></td>
                                         }
                                         <td>{usMens[i]}</td>
                                         <td>{usWomens[i]}</td>
