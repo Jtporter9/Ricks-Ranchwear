264a21dcd02d5a1916ea4d1aaffe4c29fb681e03
dakotaRuller <dakotajru@gmail.com>
2021-11-06 15:04:29 -0600

    Adding hotjar script to head

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -75,6 +75,19 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta property="og:image" content="/img/og-image.jpg" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <script>
+          {`
+          (
+          function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}
+            h._hjSettings={hjid:2683602,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+        </script>
       </Helmet>
       <Notify />
       {password !== 'twofreeboots21' && (
